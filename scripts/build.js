776900197573053b6cd3fe19d32c9b4fd3c20650
Alexander Liu <liujalexander84@gmail.com>
2021-01-01 00:38:32 -0800

    hmmmmmmmmmmmmmmmmmmmm
    i will squash rebase lol

--- scripts/build.js
+++ scripts/build.js
@@ -1,7 +1,18 @@
 // @ts-check
 
 const langs = require("./langfiles");
-const h = Object.values(langs).forEach(lang => {
-   console.log(require("../../" + lang.repopath + "/" + lang.langpath));
-   //                   ^ out of main repo      ^ other repo              ^ langfile
+
+let all = {};
+Object.values(langs).forEach(lang => {
+   /** @type {{ [key: string]: string }} */
+   const langobj = require("../../" + lang.repopath + "/" + lang.langpath);
+   //                         ^ out of main repo             ^ langfile
+
+   Object.keys(langobj).forEach(key => {
+      langobj[key] = langobj[key].toLowerCase();
+   });
+
+   Object.assign(all, langobj);
 })
+
+console.log(all);
