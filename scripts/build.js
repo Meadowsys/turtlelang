b98fe6c0c6044be49e73e49565c98eb1f4672a88
Alexander Liu <liujalexander84@gmail.com>
2021-01-01 00:33:25 -0800

    hmmmmmmmm

--- scripts/build.js
+++ scripts/build.js
@@ -1,7 +1,7 @@
 // @ts-check
 
 const langs = require("./langfiles");
-for (const lang in Object.keys(langs)) {
-   console.log(require("../../" + langs[lang].repopath + "/" + langs[lang].langpath));
+const h = Object.values(langs).forEach(lang => {
+   console.log(require("../../" + lang.repopath + "/" + lang.langpath));
    //                   ^ out of main repo      ^ other repo              ^ langfile
-}
+})
