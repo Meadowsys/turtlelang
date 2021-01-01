086eb761f05b3359c3f59c2b82dccab2e7fbe495
Alexander Liu <liujalexander84@gmail.com>
2021-01-01 00:21:54 -0800

    does this work lol?

---
+++ notes.md
@@ -0,0 +1,3 @@
+# Notes lol
+
+- add a repo to scan for, need to check `scripts/langfiles.js` and `.github/workflows/ci.yml`
---
+++ scripts/langfiles.js
@@ -0,0 +1,25 @@
+// @ts-check
+/**
+ * @type {{
+ *    [path: string]: {
+ *       repopath: string;
+ *       langpath: string;
+ *    };
+ * }}
+ */
+const langs = {
+   "minelittlepony": {
+      repopath: "minelittlepony",
+      langpath: "src/main/resources/assets/minelittlepony/lang/en_us.json"
+   },
+   "hdskins": {
+      repopath: "hdskins",
+      langpath: "src/main/resources/assets/hdskins/lang/en_us.json"
+   },
+   "presence-footsteps": {
+      repopath: "presence-footsteps",
+      langpath: "src/main/resources/assets/presencefootsteps/lang/en_us.json"
+   }
+}
+
+module.exports = langs;
